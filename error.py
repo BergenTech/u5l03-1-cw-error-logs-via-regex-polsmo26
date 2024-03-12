b518a182d438d9c1aa065146b76b037ec1c59ace
Polina <polsmo26@bergen.org>
2024-03-12 08:43:16 -0400

    assignment started

--- error.py
+++ error.py
@@ -1,1 +1,8 @@
+import json, csv
 
+with open ('logs.json', 'r') as file:
+    data = json.load(file)
+    print(data)
+    with open ('error_logs.csv', 'w') as newFile:
+        for item in data:
+            print(item['level'])
